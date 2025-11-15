56f9c43c7b2dd0af36a06e5278da5ee95ad7d667
nitishkumar07 <nitishkumarj.g07@gmail.com>
2025-11-16 00:37:19 +0530

    simple change to push

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -7,23 +7,9 @@ const Footer = () => {
       <div className="content has-text-centered">
         <p>
           <strong>Built</strong> by{" "}
-          <a href="https://tonyisern.com" target="_blank">
-            Tony Isern
+          <a href="nitishkumar07.hashnode.dev" target="_blank">
+            Nitishkumar
           </a>
-          ,{" "}
-          <a href="https://github.com/abdoul61" target="_blank">
-            Souley Abdoul Aziz
-          </a>
-          , and{" "}
-          <a href="https://github.com/elizaclamor" target="_blank">
-            Eliza Clamor
-          </a>
-          . The source code is on
-          <a href="https://github.com/OptimisticTrousers/cooking-recipe-portal">
-            {" "}
-            GitHub
-          </a>
-          .
         </p>
       </div>
     </footer>
